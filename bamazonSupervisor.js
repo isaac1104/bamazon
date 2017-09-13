9868a7ba1644f23f25064115a768a0eda7d92bc0
Isaac Kwon <you@example.com>
2017-09-12 23:29:55 -0700

    Bamazon App Finalized

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -19,8 +19,42 @@ function viewProductSales() {
         return console.log(err);
       }
       res.forEach(function(item) {
-        console.log("Department ID: " + item.department_id + " | " + "Department Name: " + item.department_name + " | " + "Overhead Costs: " + item.over_head_costs + " | " + "Product Sales: " + item.product_sales);
+        console.log("Department ID: " + item.department_id + " | " + "Department Name: " + item.department_name + " | " + "Overhead Costs: " + item.over_head_costs + " | " + "Product Sales: " + item.product_sales + " | " + " Profit: " + Number(item.product_sales - item.over_head_costs));
       });
+    });
+  });
+}
+
+function addNewDepartment() {
+  inquirer.prompt([{
+      type: "input",
+      name: "department",
+      message: "What Is The Name Of The Department You Would Like To Add?"
+    },
+    {
+      type: "input",
+      name: "overhead_costs",
+      message: "Please Set The Overhead Costs For This Department"
+    },
+    {
+      type: "input",
+      name: "product_sales",
+      message: "What Is The Product Sales Of This Department?"
+    }
+  ]).then(function(response) {
+    var department = response.department;
+    var overhead = Number(response.overhead_costs);
+    var sales = Number(response.product_sales);
+    var add = "INSERT INTO departments(department_name, over_head_costs, product_sales) VALUES ?";
+    var value = [
+      [department, overhead, sales]
+    ];
+
+    connection.query(add, [value], function(err, res) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("You have successfully added: " + department + " department " + "with $" + overhead + " overhead costs" + " that has $" + sales + " in product sales");
     });
   });
 }
@@ -37,6 +71,8 @@ connection.connect(function(err) {
   }]).then(function(response) {
     if (response.list === "View Product Sales by Department") {
       viewProductSales();
+    } else if (response.list === "Add New Department") {
+      addNewDepartment();
     }
   });
 });
